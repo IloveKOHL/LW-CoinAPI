7a078980dd0d1e25a98d66857e713856d98ba906
Markensau <oetkenlukas@gmail.com>
2021-12-10 10:48:49 +0100

    Commands Finished

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # LW-CoinAPI
-**Progess 80%**
+
+Das LW-CoinAPI Plugin ist für Minecraft Spigot ausgelegt und bietet eine Große
+funktionalität.
+Es beinhaltet eine vielfältige API, die unten näher erklärt wird.
+Fast alles ist in der ```config.yml``` einstellbar.
+Die MySQL verbindung erfolgt nach den einstellen der ```mysql.yml```.
+
+## Spieler-Befehle
+
+```
+Command                                   | Permission              | Funktion
+
+/coins                                    | -------------           | Zeigt dir deinen aktuellen Coins
+```
+
+
+## Admin-Befehle
+
+```
+Command                                         | Permission              | Funktion
+
+/coins <player>                                 | lwc.seeother            | Zeigt dir die Coins eines anderen Spielers
+/coins <set | add | remove> <Player> <Amount>   | lwc.admin               | Setzt / Gibt / Nimmt einen Spieler seine Coins
+```
+
+
+## Konfigurationshilfe
+
+### config.yml
+```
+// Prefix
+// FirstJoinCoins, heißt wie viele Coins soll ein Spieler erhalten wenn er zum ersten mal Join min. 1
+
+Prefix: '&8[&5LW&8-&3Coins&8]'
+FirstJoinCoins: 10
+
+```
+
+### MySQL.yml
+
+```
+// use MUSS auf true gestzt werden
+
+mysql:
+  use: true
+  host: 0.0.0.0
+  port: 3306
+  user: <user>
+  datenbase: <database>
+  passwort: ******
+  
+```
+
+# Viel Spaß wünschen
+## LvckyAPI & IloveKOHL | Developer
+
--- src/main/java/net/lvckyworld/coin/commands/Coins.java
+++ src/main/java/net/lvckyworld/coin/commands/Coins.java
@@ -30,7 +30,12 @@ public class Coins implements CommandExecutor {
                     p.sendMessage(LWCoins.prefix + "§7Der Spieler §3" + args[0] + "§7 hat §6" + coins + "§7 Coins.");
                 } else {
                     Long coins = LWCoinsAPI.getOfflinePlayerCoins(args[0]);
-                    p.sendMessage(LWCoins.prefix + "§7Der Spieler §3" + args[0] + "§7 hat §6" + coins + "§7 Coins.");
+                    if (coins != null) {
+                        p.sendMessage(LWCoins.prefix + "§7Der Spieler §3" + args[0] + "§7 hat §6" + coins + "§7 Coins.");
+                    } else {
+                        p.sendMessage(LWCoins.prefix + "§4Da der Spieler offline ist muss er min. 1 Connected sein, um in der DatenBank registriert zu werden.");
+                        p.sendMessage(" ");
+                    }
                 }
             } else {
                 p.sendMessage(LWCoins.prefix + "§4Dazu fehlt dir die Permission: " + "lwc.seeother");
@@ -40,50 +45,55 @@ public class Coins implements CommandExecutor {
         } else if (args.length == 3) {
             // /coins <set | add | remove> <Player> <Amount>
             if (p.hasPermission("lwc.admin")) {
-                Player target = Bukkit.getPlayer(args[1]);
-                try {Long.parseLong(args[2]);} catch (Exception e) {p.sendMessage(LWCoins.prefix + "§4Wrong usage: /coins <set | add | remove> <Player> <Amount>");}
-                if (target == null) {
-                    p.sendMessage(LWCoins.prefix + "§4Da der Spieler offline ist muss er min. 1 Connected sein, um in der DatenBank registriert zu werden.");
-                    p.sendMessage(" ");
-                }
-                // SET
-                if (args[0].equalsIgnoreCase("set")) {
-                    if (target != null) {
-                        LWCoinsAPI.update(target, Long.parseLong(args[2]));
-                        p.sendMessage(LWCoins.prefix + "§7Der Spieler §3" + args[1] + "§7 hat jetzt §6" + args[2] + "§7 Coins.");
-                    } else {
-                        LWCoinsAPI.updateOffline(args[1], Long.parseLong(args[2]));
-                        p.sendMessage(LWCoins.prefix + "§7Der Spieler §3" + args[1] + "§7 hat jetzt §6" + args[2] + "§7 Coins.");
+                if (args[0].equalsIgnoreCase("set") || args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("remove")) {
+                    Player target = Bukkit.getPlayer(args[1]);
+                    try {Long.parseLong(args[2]);} catch (Exception e) {p.sendMessage(LWCoins.prefix + "§4Wrong usage: /coins <set | add | remove> <Player> <Amount>");}
+                    if (target == null) {
+                        p.sendMessage(LWCoins.prefix + "§4Da der Spieler offline ist muss er min. 1 Connected sein, um in der DatenBank registriert zu werden.");
+                        p.sendMessage(" ");
                     }
-                }
-                // ADD
-                if (args[0].equalsIgnoreCase("add")) {
-                    if (target != null) {
-                        Long coins = LWCoinsAPI.getCoins(target);
-                        long newCoins = coins + Long.parseLong(args[2]);
-                        LWCoinsAPI.update(target, newCoins);
-                        p.sendMessage(LWCoins.prefix + "§7Der Spieler §3" + args[1] + "§7 hat jetzt §6" + newCoins + "§7 Coins.");
-                    } else {
-                        Long coins = LWCoinsAPI.getOfflinePlayerCoins(args[1]);
-                        long newCoins = coins + Long.parseLong(args[2]);
-                        LWCoinsAPI.updateOffline(args[1], newCoins);
-                        p.sendMessage(LWCoins.prefix + "§7Der Spieler §3" + args[1] + "§7 hat jetzt §6" + newCoins + "§7 Coins.");
+                    // SET
+                    if (args[0].equalsIgnoreCase("set")) {
+                        if (target != null) {
+                            LWCoinsAPI.update(target, Long.parseLong(args[2]));
+                            p.sendMessage(LWCoins.prefix + "§7Der Spieler §3" + args[1] + "§7 hat jetzt §6" + args[2] + "§7 Coins.");
+                        } else {
+                            LWCoinsAPI.updateOffline(args[1], Long.parseLong(args[2]));
+                            p.sendMessage(LWCoins.prefix + "§7Der Spieler §3" + args[1] + "§7 hat jetzt §6" + args[2] + "§7 Coins.");
+                        }
                     }
-                }
-                // REMOVE
-                if (args[0].equalsIgnoreCase("remove")) {
-                    if (target != null) {
-                        Long coins = LWCoinsAPI.getCoins(target);
-                        long newCoins = coins - Long.parseLong(args[2]);
-                        LWCoinsAPI.update(target, newCoins);
-                        p.sendMessage(LWCoins.prefix + "§7Der Spieler §3" + args[1] + "§7 hat jetzt §6" + newCoins + "§7 Coins.");
-                    } else {
-                        Long coins = LWCoinsAPI.getOfflinePlayerCoins(args[1]);
-                        long newCoins = coins - Long.parseLong(args[2]);
-                        LWCoinsAPI.updateOffline(args[1], newCoins);
-                        p.sendMessage(LWCoins.prefix + "§7Der Spieler §3" + args[1] + "§7 hat jetzt §6" + newCoins + "§7 Coins.");
+                    // ADD
+                    if (args[0].equalsIgnoreCase("add")) {
+                        if (target != null) {
+                            Long coins = LWCoinsAPI.getCoins(target);
+                            long newCoins = coins + Long.parseLong(args[2]);
+                            LWCoinsAPI.update(target, newCoins);
+                            p.sendMessage(LWCoins.prefix + "§7Der Spieler §3" + args[1] + "§7 hat jetzt §6" + newCoins + "§7 Coins.");
+                        } else {
+                            Long coins = LWCoinsAPI.getOfflinePlayerCoins(args[1]);
+                            long newCoins = coins + Long.parseLong(args[2]);
+                            LWCoinsAPI.updateOffline(args[1], newCoins);
+                            p.sendMessage(LWCoins.prefix + "§7Der Spieler §3" + args[1] + "§7 hat jetzt §6" + newCoins + "§7 Coins.");
+                        }
                     }
+                    // REMOVE
+                    if (args[0].equalsIgnoreCase("remove")) {
+                        if (target != null) {
+                            Long coins = LWCoinsAPI.getCoins(target);
+                            long newCoins = coins - Long.parseLong(args[2]);
+                            LWCoinsAPI.update(target, newCoins);
+                            p.sendMessage(LWCoins.prefix + "§7Der Spieler §3" + args[1] + "§7 hat jetzt §6" + newCoins + "§7 Coins.");
+                        } else {
+                            Long coins = LWCoinsAPI.getOfflinePlayerCoins(args[1]);
+                            long newCoins = coins - Long.parseLong(args[2]);
+                            LWCoinsAPI.updateOffline(args[1], newCoins);
+                            p.sendMessage(LWCoins.prefix + "§7Der Spieler §3" + args[1] + "§7 hat jetzt §6" + newCoins + "§7 Coins.");
+                        }
+                    }
+                } else {
+                    p.sendMessage(LWCoins.prefix + "§4Wrong usage /coins <set | add | remove> <Player> <Amount>");
                 }
+
             } else {
                 p.sendMessage(LWCoins.prefix + "§4Dazu fehlt dir die Permission: " + "lwc.admin");
             }
